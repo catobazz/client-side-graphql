0ae429156747de25718517231590af15d82c64d1
Artem Rybin <catobazzz@gmail.com>
2025-04-24 16:17:13 +0300

    feat: add apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import GlobalStyles from './styles';
 import Pages from './pages';
 
+
 const root = createRoot(document.getElementById('root')!);
 
 root.render(
